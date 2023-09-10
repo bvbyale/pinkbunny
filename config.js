22591c9b62922a4a1dc3e702e2bf7f0973a3abc2
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-10 14:03:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['51992004117', '👑 Bruno sobrino - Creador 👑', true],
-  ['50576390682', '💫 Ender GB - Collaborator 1 💫', true],
+  ['51992004117', '👑 Creador 👑', true],
+  ['50576390682', '💫 Colaborador 1 💫', true],
 ];
 
 global.suittag = ['5219993404349'];
