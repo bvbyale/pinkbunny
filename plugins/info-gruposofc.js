010b019d0dedb1669df7ea30bebe00f4df48aa92
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-10 17:41:00 -0500

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,11 +3,12 @@ const handler = async (m, {conn, usedPrefix}) => {
   const document = doc[Math.floor(Math.random() * doc.length)];
   const text = `*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃-𝙼𝙳 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 
-*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙲:* 
+*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝙾𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝙳𝚎𝚕 𝙱𝚘𝚝:*
+
+✪ *𝙶𝚁𝚄𝙿𝙾 𝙾𝙵𝙲:* 
 ༄ ${gp1} ༆
 
-*𝙶𝚁𝚄𝙿𝙾 𝚂𝚄𝙿𝙿𝙾𝚁𝚃*
+✪ *𝙶𝚁𝚄𝙿𝙾 𝚂𝚄𝙿𝙿𝙾𝚁𝚃*
 ༄ ${gp2} ༄
 `.trim();
   const buttonMessage= {
