2c073a62747ba3b9176cdf4452eb2016fbb2e96e
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-11 15:54:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('✅ㅤIniciando...');
+console.log('✅ㅤIniciando Hachiko-Bot');
 import {join, dirname} from 'path';
 import {createRequire} from 'module';
 import {fileURLToPath} from 'url';
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
+say('Hachiko - Bot\n MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`By Hache Jota`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
