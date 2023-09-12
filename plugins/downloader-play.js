14df26cdf3fcc6707047957909cce9d1171305ad
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-12 17:46:05 -0500

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -16,14 +16,14 @@ const handler = async (m, {conn, command, args, text, usedPrefix}) => {
     } else if (command === 'play2') {
       additionalText = 'video 🎥';
     }
-    const texto1 = `*◉—⌈🔊 𝕐𝕆𝕌𝕋𝕌𝔹𝔼 𝕄𝕌𝕊𝕀ℂ𝔸 🔊⌋—◉*\n
-❏ 🔰 *𝚃𝙸𝚃𝚄𝙻𝙾:* ${yt_play[0].title}
-❏ ⌛ *𝙳𝚄𝚁𝙰𝙲𝙸𝙾𝙽:* ${secondString(yt_play[0].duration.seconds)}
-❏ 👀 *𝚅𝙸𝚂𝚃𝙰𝚂:* ${`${MilesNumber(yt_play[0].views)}`}
-❏ 👤 *𝙰𝚄𝚃𝙾𝚁:* ${yt_play[0].author.name}
-❏ 📺 *𝙲𝙰𝙽𝙰𝙻:* ${yt_play[0].author.url}
-❏ 🔗 *𝙻𝙸𝙽𝙺:* ${yt_play[0].url}\n
-❏ *_Enviando ${additionalText}, aguarde un momento．．．_*`.trim();
+    const texto1 = `*◉—⌈🔊 𝕐𝕆𝕌𝕋𝕌𝔹𝔼 ℙ𝕃𝔸𝕐 🔊⌋—◉*\n
+┣⟣☘︎ 🔰 *𝚃𝙸𝚃𝚄𝙻𝙾:* ${yt_play[0].title}
+┣⟣☘︎ ⌛ *𝙳𝚄𝚁𝙰𝙲𝙸𝙾𝙽:* ${secondString(yt_play[0].duration.seconds)}
+┣⟣☘︎ 👀 *𝚅𝙸𝚂𝚃𝙰𝚂:* ${`${MilesNumber(yt_play[0].views)}`}
+┣⟣☘︎ 👤 *𝙰𝚄𝚃𝙾𝚁:* ${yt_play[0].author.name}
+┣⟣☘︎ 📺 *𝙲𝙰𝙽𝙰𝙻:* ${yt_play[0].author.url}
+┣⟣☘︎ 🔗 *𝙻𝙸𝙽𝙺:* ${yt_play[0].url}\n
+┣⟣☘︎ *_Enviando ${additionalText}, aguarde un momento．．．_*`.trim();
     conn.sendMessage(m.chat, {image: {url: yt_play[0].thumbnail}, caption: texto1}, {quoted: m});
     if (command == 'play') {
       try {      
