3b3e49ded0f85652ccc31db56adaa87f26472146
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-12 18:57:57 -0600

    Update menu-juegos.js

--- plugins/menu-juegos.js
+++ plugins/menu-juegos.js
@@ -35,39 +35,39 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┏━━━━━━━━━━━━━━━━┓
 ┃ *< 𝕁𝕌𝔼𝔾𝕆𝕊 />*
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃ 
-┣⟣❥ _${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
-┣⟣❥ _${usedPrefix}fake *<texto1> <@tag> <texto2>*_
-┣⟣❥ _${usedPrefix}ppt *<papel / tijera /piedra>*_
-┣⟣❥ _${usedPrefix}prostituto *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}prostituta *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}gay2 *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}lesbiana *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}pajero *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}pajera *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}puto *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}puta *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}manco *<nombre / @tag>*_
-┣⟣❥ _${usedPrefix}manca *<nombre / @tag>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}rata *<nombre / @tag>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}love *<nombre / @tag>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}doxear *<nombre / @tag>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}pregunta *<texto>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}suitpvp *<@tag>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}slot *<apuesta>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}ttt *<nombre sala>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}delttt_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}acertijo_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}simi *<texto>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}top *<texto>*_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}topgays_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}topotakus_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}formarpareja_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}verdad_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}reto_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}cancion_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}pista_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}akinator_
- ⟣❥ ඬ⃟ 🏰 _${usedPrefix}wordfind_
+┣⟣❥  🏰 _${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
+┣⟣❥  🏰 _${usedPrefix}fake *<texto1> <@tag> <texto2>*_
+┣⟣❥  🏰 _${usedPrefix}ppt *<papel / tijera /piedra>*_
+┣⟣❥  🏰 _${usedPrefix}prostituto *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}prostituta *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}gay2 *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}lesbiana *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}pajero *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}pajera *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}puto *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}puta *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}manco *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}manca *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}rata *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}love *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}doxear *<nombre / @tag>*_
+┣⟣❥  🏰 _${usedPrefix}pregunta *<texto>*_
+┣⟣❥  🏰 _${usedPrefix}suitpvp *<@tag>*_
+┣⟣❥  🏰 _${usedPrefix}slot *<apuesta>*_
+┣⟣❥  🏰 _${usedPrefix}ttt *<nombre sala>*_
+┣⟣❥  🏰 _${usedPrefix}delttt_
+┣⟣❥  🏰 _${usedPrefix}acertijo_
+┣⟣❥  🏰 _${usedPrefix}simi *<texto>*_
+┣⟣❥  🏰 _${usedPrefix}top *<texto>*_
+┣⟣❥  🏰 _${usedPrefix}topgays_
+┣⟣❥  🏰 _${usedPrefix}topotakus_
+┣⟣❥  🏰 _${usedPrefix}formarpareja_
+┣⟣❥  🏰 _${usedPrefix}verdad_
+┣⟣❥  🏰 _${usedPrefix}reto_
+┣⟣❥  🏰 _${usedPrefix}cancion_
+┣⟣❥  🏰 _${usedPrefix}pista_
+┣⟣❥  🏰 _${usedPrefix}akinator_
+┣⟣❥  🏰 _${usedPrefix}wordfind_
 ┗━━━━━━━━━━━━━━━━┛`.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
