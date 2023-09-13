722748672b06ca5f18f37503104af2a15b788862
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-13 13:47:54 -0500

    Update menu-prue.js

--- plugins/menu-prue.js
+++ plugins/menu-prue.js
@@ -371,7 +371,7 @@ const handler = async (m, { conn, args }) => {
 *┃❍ 𝕆𝕎ℕ𝔼ℝ𝕊 𝕐 𝕄𝕆𝔻𝔼ℝ𝔸𝕋𝕆ℝ𝕊 ❍*
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
 ┣⟣❥ _${usedPrefix}menuowner_
-┗━━━━━━━━━━━━━━━━┛ `;
+┗━━━━━━━━━━━━━━━━┛ `.trim();
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": global.md, "sourceUrl": global.md}}}, {quoted: m});
 };
 handler.help = ['menu'];
