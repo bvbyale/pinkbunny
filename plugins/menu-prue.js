cba8b7f28c328bcf3dff1eb61a15ea1ccac15716
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-09-12 22:22:45 -0500

    Update menu-prue.js

--- plugins/menu-prue.js
+++ plugins/menu-prue.js
@@ -6,7 +6,8 @@ const handler = async (m, { conn, args }) => {
    let txt = `  𝙼𝙴𝙽𝚄 𝙿𝚁𝚄𝙴𝙱𝙰 𝚇𝙳  `;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": global.md, "sourceUrl": global.md}}}, {quoted: m});
 };
-handler.help = ['scbot'];
-handler.tags = ['info'];
-handler.command = /^(m1)$/i;
+catch {
+    conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
+  };
+handler.command = /^(menu+)$/i;
 export default handler;
